034dff9f35724fc615195c01a4021717a5fcad49
anjalei <anjalirnair302@gmail.com>
2024-10-01 22:08:05 -0700

    jhioj

---
+++ form.js
@@ -0,0 +1,25 @@
+function Person(firstName, lastName, age, sex) {
+    this.age = age;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.sex = sex;
+    this.printFullName = function () {
+          console.log(this.firstName + " " + this.lastName);
+    };
+  }
+  
+  Person.prototype.findegligibleOrNot = function findegligibleOrNot(minAge) {
+      console.log(this.person.age >= 18 ? "ELIGIBLE" : "NOT ELIGIBLE");
+  };
+  
+  
+  function createNewStudents(){
+      const person1 = new Person("yash", "prasad", 10, "M");
+      const person2 = new Person("vaibhav", "prasad", 20, "M");
+      
+      person1.printFullName();
+      person1.findegligibleOrNot(18);
+      person2.printFullName();
+      person2.findegligibleOrNot(18);
+  
+  }
